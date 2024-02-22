65dd8eb161c5654f8efe79253314d122f5c6c07d
Kirk Thomas <dablumaroon@gmail.com>
2024-02-22 07:39:37 -0500

    Changing import to require

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
-import { ApolloServer } from "apollo-server-express";
-import express from "express";
+const { ApolloServer } = require("apollo-server-express");
+const express = require("express");
 const { expressjwt: jwt } = require("express-jwt");
 
-import resolvers from "./schemas/resolvers.js";
-import typedefs  from "./schemas/typedefs.js";
+const resolvers = require("./schemas/resolvers.js");
+const typedefs = require("./schemas/typedefs.js");
 
 const port = 3000;
 const app = express();
 
 app.use(
-    jwt(
-        {
-            secret: "HUSH_YO_MOUTH",
-            algorithms: ["HS256"],
-            credentialsRequired: false
-        }
-    )
+  jwt({
+    secret: "HUSH_YO_MOUTH",
+    algorithms: ["HS256"],
+    credentialsRequired: false,
+  })
 );
 
 const server = new ApolloServer({
-    typedefs,
-    resolvers,
-    context: ({ req }) => {
-        const user = req.user || null;
-        return {user};
-    }
+  typedefs,
+  resolvers,
+  context: ({ req }) => {
+    const user = req.user || null;
+    return { user };
+  },
 });
 
 server.applyMiddleware({ app });
 
-app.listen({ port }, () =>{
-    console.log(`Your server is ready on http://localhost:${port}${server.graphqlPath}`);
+app.listen({ port }, () => {
+  console.log(
+    `Your server is ready on http://localhost:${port}${server.graphqlPath}`
+  );
 });
